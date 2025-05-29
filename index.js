2e3cd2ba3a28e7d1b9212af4f3a6c7b45559e436
Phowk <estheban1023@gmail.com>
2025-05-29 14:16:19 -0500

    Refactor code structure for improved readability and maintainability

--- index.js
+++ index.js
@@ -1,55 +1,24 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const botonVerMas = document.getElementById("botonvermas");
-    const contenedorAbajo = document.getElementById("contenedor2");
-    const modal = document.getElementById("modal");
-    const cerrarBtn = document.getElementById("cerrarModal");
 
-    botonVerMas.addEventListener("click", () => {
-        // Desplazar hacia el contenedor de abajo
-        contenedorAbajo.scrollIntoView({ behavior: "smooth", block: "start" });
+document.addEventListener('DOMContentLoaded', function() {
+  const btn = document.getElementById('btnScrollTop');
+const regresar = document.querySelector('.enlace-lista');
 
-        // Agregar clase para resaltar
-        contenedorAbajo.classList.add("resaltar");
-
-        // Quitar el resaltado después de 1 segundo
-        setTimeout(() => {
-            contenedorAbajo.classList.remove("resaltar");
-        }, 700);
-    });
-    // Para mostrar el modal desde otro botón (ejemplo)
-  const mostrarVida = document.getElementById("vida");
-  // if (mostrarVida) {
-  //   mostrarVida.addEventListener("click", () => {
-  //       modal.style.display = "flex";
-  //       modal.style.animation = "fadeIn 0.5s";
-  //       modal.style.animationFillMode = "forwards";
-  //       document.getElementById("titulo").textContent = "Su vida";
-
-  //   });
-  // }
-  const mostrarObra = document.getElementById("obra");
-  if (mostrarObra) {
-    mostrarObra.addEventListener("click", () => {
-        modal.style.display = "flex";
-        modal.style.animation = "fadeIn 0.5s";
-        modal.style.animationFillMode = "forwards";
-        document.getElementById("titulo").textContent = "Su Obra";
-
-    });
+  function isOutOfView(element) {
+    const rect = element.getBoundingClientRect();
+    return rect.bottom < 0;
   }
 
-  const mostrarsuperspectiva = document.getElementById("perspectiva");
-  if (mostrarsuperspectiva) {
-    mostrarsuperspectiva.addEventListener("click", () => {
-        modal.style.display = "flex";
-        modal.style.animation = "fadeIn 0.5s";
-        modal.style.animationFillMode = "forwards";
-        document.getElementById("titulo").textContent = "Su perspectiva política";
+  window.addEventListener('scroll', function() {
+    if (isOutOfView(regresar)) {
+      btn.style.display = 'block';
+    } else {
+      btn.style.display = 'none';
+    }
+  });
 
-    });
-  }
-
-
-
+  btn.addEventListener('click', function() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  });
 });
+
 
